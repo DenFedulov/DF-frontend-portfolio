062decb5e8f74c3ce8bd6619d1ec6d1ef06ad458
DenFedulov <den.fedulov.1999@mail.ru>
2022-07-26 18:02:16 +0300

    9th tasks

--- scripts/source_code_replacer.js
+++ scripts/source_code_replacer.js
@@ -4,45 +4,7 @@ const headerPromise = replaceHTML("/views/homeheader.html", ".page_start");
 const codePromise = replaceHTML("/views/code_sections.html", ".page_end");
 
 placeHTMLSourceCode();
-
-function toggleSection(className) {
-    for (const i of document.getElementsByClassName(className)) {
-        i.classList.toggle('off');
-    }
-}
-
-async function placeHTMLSourceCode() {
-    await codePromise;
-    try {
-        document.querySelector('.html_code').innerText = document.querySelector('.page').innerHTML;
-    } catch (e) {
-        console.error(e);
-        document.querySelector('.html_code').innerText = "Error loading file: " + e;
-    }
-}
-
-async function replaceCodeSection(path, target) {
-    await codePromise;
-    let targetElem;
-    try {
-        let fullPath = location.origin + path;
-        targetElem = document.getElementsByClassName(target)[0];
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.open('GET', fullPath, true);
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                targetElem.innerHTML = "<br>";
-                targetElem.innerText += this.responseText;
-            }
-        }
-
-        xhttp.send();
-    } catch (e) {
-        console.error(e);
-        if (targetElem) targetElem.innerText = "Error loading file: " + e;
-    };
-}
+addTitle();
 
 function replaceHTML(path, target) {
     return new Promise((resolve, reject) => {
@@ -64,4 +26,60 @@ function replaceHTML(path, target) {
             console.error(e);
         }
     })
+}
+
+async function placeHTMLSourceCode() {
+    await codePromise;
+    try {
+        document.querySelector('.html_code').innerText = document.querySelector('.page').innerHTML;
+    } catch (e) {
+        console.error(e);
+        document.querySelector('.html_code').innerText = "Error loading file: " + e;
+    }
+}
+
+function addTitle() {
+
+    let taskName = "Задача " + document.location.pathname.split('/').pop().match('.*(?=\\.)')[0];
+    const head = document.querySelector('head');
+    const page = document.querySelector('.page');
+
+    console.log(head);
+
+    const h1 = document.createElement('h1');
+    const title = document.createElement('title');
+    h1.innerText = taskName;
+    title.innerText = taskName;
+
+    head.append(title);
+    page.prepend(h1);
+}
+
+function toggleSection(className) { // Used in /views/code_sections.html 
+    for (const i of document.getElementsByClassName(className)) {
+        i.classList.toggle('off');
+    }
+}
+
+async function replaceCodeSection(path, targetElem) { // Used inside html in <script> to choose which code section to place
+    await codePromise;
+    let target;
+    try {
+        let fullPath = location.origin + path;
+        target = document.getElementsByClassName(targetElem)[0];
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.open('GET', fullPath, true);
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                target.innerHTML = "<br>";
+                target.innerText += this.responseText;
+            }
+        }
+
+        xhttp.send();
+    } catch (e) {
+        console.error(e);
+        if (target) target.innerText = "Error loading file: " + e;
+    };
 }
