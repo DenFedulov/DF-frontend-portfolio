246fc8d9167b3b2260b7d06186992dabd2e9a2e7
DenFedulov <den.fedulov.1999@mail.ru>
2022-07-26 18:07:42 +0300

    sidebar toggle fix

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ const app_frame = window.parent.document.querySelector('.app_frame');
 
 function load(target, path) {
     if (path !== location.pathname) {
-        if (window === window.parent) toggleSidebar(true);
+        if (window === window.parent && target === 'sidebar') toggleSidebar(true);
         if (target === 'sidebar') {
             sidebar_frame.src = path;
         }
