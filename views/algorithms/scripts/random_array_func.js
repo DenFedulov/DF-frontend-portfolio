c8e3d3ca57621e056ea80e467ee85f7ad5b1a208
DenFedulov <den.fedulov.1999@mail.ru>
2022-08-10 23:40:57 +0300

    quick sort almost done

--- views/algorithms/scripts/random_array_func.js
+++ views/algorithms/scripts/random_array_func.js
@@ -1,18 +1,43 @@
 "use strict";
 
-function quickSort(arr) {
+function quickSort(arr, arrStart = 0, arrEnd = arr.length) {
 
-    if (arr.length > 3) {
-        let pivot = pickPivotAndPrepareArray(arr);
+    if (arrEnd - arrStart > 3) {
+        let pivot = pickPivotAndPrepareArray(arr); //issue is here
 
+        console.log(arr[pivot]);
 
+        swapValuesInArray(arr, pivot, arrEnd - 1);
 
+        while (true) {
+            let largerIndFromLeft = -1;
+            let smallerIndFromRight = -1;
 
+            for (let l = arrStart; l < arrEnd - 1; l++) {
+                if (arr[l] > arr[arrEnd - 1]) {
+                    largerIndFromLeft = l;
+                    break;
+                }
+            }
+            for (let r = arrEnd - 2; r > arrStart; r--) {
+                if (arr[r] < arr[arrEnd - 1]) {
+                    smallerIndFromRight = r;
+                    break;
+                }
+            }
 
-        return arr[pivot];
+            if (largerIndFromLeft <= smallerIndFromRight) {
+                swapValuesInArray(arr, largerIndFromLeft, smallerIndFromRight);
+            } else {
+                swapValuesInArray(arr, largerIndFromLeft, arrEnd - 1);
+                break;
+            }
+        }
+
+        return arr;
     }
 
-    return NaN;
+    return simpleSort(arr);
 }
 
 function pickPivotAndPrepareArray(arr) {
@@ -21,21 +46,29 @@ function pickPivotAndPrepareArray(arr) {
     pivotOptions.push(arr[Math.floor((arr.length - 1) / 2)]);
     pivotOptions.push(arr[arr.length - 1]);
 
-    for (let j = 0; j < pivotOptions.length; j++) {
-        for (let i = 0; i < pivotOptions.length; i++) {
-            if (pivotOptions[i] > pivotOptions[i + 1]) {
-                let plhdr = pivotOptions[i];
-                pivotOptions[i] = pivotOptions[i + 1];
-                pivotOptions[i + 1] = plhdr;
-            }
-        }
-    }
+    simpleSort(pivotOptions);
 
     arr[0] = pivotOptions[0];
     arr[Math.floor((arr.length - 1) / 2)] = pivotOptions[1];
     arr[arr.length - 1] = pivotOptions[2];
 
     return Math.floor((arr.length - 1) / 2);
+}
+
+function swapValuesInArray(arr, firstIndex, secondIndex) {
+    let placeholder = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = placeholder;
+}
+
+function simpleSort(arr) {
+    for (let j = 0; j < arr.length; j++) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swapValuesInArray(arr, i, i + 1);
+            }
+        }
+    }
 }
 
 function randomInt(min, max) {
@@ -49,9 +82,9 @@ function generateRandomArray(size, min, max, isSorted = false) {
         arr.push(randomInt(min, max));
     }
 
-    console.log(quickSort(arr));
+    quickSort(arr);
 
     return arr;
 }
 
-console.log(generateRandomArray(10, 0, 100, true));
+console.log(generateRandomArray(10, 0, 100));
