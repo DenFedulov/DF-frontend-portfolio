1eeec3865378ada15e4bbcd5ff1a24ceba924548
DenFedulov <den.fedulov.1999@mail.ru>
2022-08-10 21:19:14 +0300

    quick sort progress

--- views/algorithms/scripts/random_array_func.js
+++ views/algorithms/scripts/random_array_func.js
@@ -1,21 +1,57 @@
 "use strict";
 
+function quickSort(arr) {
 
+    if (arr.length > 3) {
+        let pivot = pickPivotAndPrepareArray(arr);
+
+
+
+
+
+        return arr[pivot];
+    }
+
+    return NaN;
+}
+
+function pickPivotAndPrepareArray(arr) {
+    let pivotOptions = [];
+    pivotOptions.push(arr[0]);
+    pivotOptions.push(arr[Math.floor((arr.length - 1) / 2)]);
+    pivotOptions.push(arr[arr.length - 1]);
+
+    for (let j = 0; j < pivotOptions.length; j++) {
+        for (let i = 0; i < pivotOptions.length; i++) {
+            if (pivotOptions[i] > pivotOptions[i + 1]) {
+                let plhdr = pivotOptions[i];
+                pivotOptions[i] = pivotOptions[i + 1];
+                pivotOptions[i + 1] = plhdr;
+            }
+        }
+    }
+
+    arr[0] = pivotOptions[0];
+    arr[Math.floor((arr.length - 1) / 2)] = pivotOptions[1];
+    arr[arr.length - 1] = pivotOptions[2];
+
+    return Math.floor((arr.length - 1) / 2);
+}
 
 function randomInt(min, max) {
     return Math.floor((max + 1 - min) * Math.random() + min);
 }
 
 function generateRandomArray(size, min, max, isSorted = false) {
-    let array = [];
+    let arr = [];
 
     for (let i = 0; i < size; i++) {
-        array.push(randomInt(min, max));
+        arr.push(randomInt(min, max));
     }
 
+    console.log(quickSort(arr));
 
-
-    return array;
+    return arr;
 }
 
-console.log(generateRandomArray(20, 5, 100));
+console.log(generateRandomArray(10, 0, 100, true));
