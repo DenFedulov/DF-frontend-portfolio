efe35c4e8731ca40d00c6abe1f6917904452ba07
DenFedulov <den.fedulov.1999@mail.ru>
2022-08-19 15:40:22 +0300

    dynamic task placer

--- scripts/task_placer.js
+++ scripts/task_placer.js
@@ -4,7 +4,7 @@ class TaskPlacer {
 
     constructor() {
         this.sectionElems = document.querySelectorAll('.section');
-        this.tasksFolderPath = '/views/tasks/';
+        this.tasksFolderPath = '/views/learn_js_ru_tasks/tasks/';
     }
 
     async init() {
@@ -16,22 +16,7 @@ class TaskPlacer {
         }
 
         sectionsToTasksMap.forEach((tasks, section) => {
-            const ulElem = document.createElement('ul');
-            section.append(ulElem);
-            for (let i = 0; i < tasks.length; i++) {
-                const taskName = tasks[i];
-                const liElem = document.createElement('li');
 
-                let fullPath = this.tasksFolderPath + taskName + '.html';
-
-                let link = this.createLinkElement(fullPath, taskName);
-                let directLink = createDirectLinkElement(fullPath);
-
-                liElem.append(link);
-                liElem.append(directLink);
-
-                ulElem.append(liElem);
-            }
         });
     }
 
@@ -51,19 +36,42 @@ class TaskPlacer {
         return resp.status == 200 ? true : false;
     }
 
+    renderSection(section, tasks) {
+        const ulElem = document.createElement('ul');
+        section.append(ulElem);
+
+        for (let i = 0; i < tasks.length; i++) {
+            const taskName = tasks[i];
+            const liElem = document.createElement('li');
+
+            let fullPath = this.tasksFolderPath + taskName + '.html';
+
+            let link = this.createLinkElement(fullPath, taskName);
+            let directLink = createDirectLinkElement(fullPath);
+
+            liElem.append(link);
+            liElem.append(directLink);
+
+            ulElem.append(liElem);
+        }
+    }
 
     async getExistingTasks() {
-        let existingTasks = [];
+        const existingTasks = [];
         let checkPromise;
+        const sectionsToTasksMap = new Map();
 
         for (let taskNum = 1; await this.checkForTask(taskNum + '-' + 1) || taskNum == 1; taskNum++) {
-            let existingSection = [];
+            let existingSectionTasks = [];
             for (let taskCount = 1; await checkPromise || taskCount == 1; taskCount++) {
                 let taskName = taskNum + '-' + taskCount;
                 checkPromise = this.checkForTask(taskName);
-                checkPromise.then((value) => { if (value) { existingSection.push(taskName) } });
+                checkPromise.then((value) => { if (value) { existingSectionTasks.push(taskName) } });
             }
-            existingTasks.push(existingSection);
+            existingTasks.push(existingSectionTasks);
+
+            this.renderSection(this.sectionElems[taskNum - 1], existingSectionTasks)
+
             console.clear();
         }
         console.clear();
